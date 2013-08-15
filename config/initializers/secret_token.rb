57de1440d67d9e35e3d46a6727c40a90219ba23a
Rosario Martinez <rosario.mgomez@gmail.com>
2013-08-15 09:50:09 -0700

    concept app for schedules

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EventScheduler::Application.config.secret_token = '3dc4598b6e5f83641abeed361c1b47cf30c3521902a5b29a5a0505a112c8cb7ab591ac364c451d1d152255938e4a42479641a9de137df019d5949165be874271'
